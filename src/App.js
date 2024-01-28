74b497951a5bd8775f7cbef17bb8cb8e21c6e074
sainesh25 <patilsainesh25@gmail.com>
2024-01-29 02:12:29 +0530

    button logics updated

--- src/App.js
+++ src/App.js
@@ -29,6 +29,11 @@ function App() {
     const handleAdd = () => {
         let answer_of_operation = +number1 + +number2
         setAnswer(`Addition of ${number1} and ${number2} is ${answer_of_operation}`);
+        setInput({
+            ...input,
+            firstInput: '',
+            secondInput: '',
+        });
     }
 
     const handleSub = () => {
@@ -40,12 +45,19 @@ function App() {
         let answer_of_operation = +number1 * +number2
         setAnswer(`Multiplication of ${number1} and ${number2} is ${answer_of_operation}`);
     }
-    
+
     const handleDiv = () => {
         let answer_of_operation = +number1 / +number2
         setAnswer(`Dividation of ${number1} and ${number2} is ${answer_of_operation}`);
     }
 
+    const handleInput = (e) => {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        })
+        console.log(e.target.value);
+    }
 
 
     return (
@@ -57,6 +69,21 @@ function App() {
             <Button onclick={handleSub}>Subtract</Button>
             <Button onclick={handleDiv}>Divide</Button>
             <Button onclick={handleMul}>Multiply</Button>
+
+            <div >
+
+                <Button onclick={handleInput} name='0' value='0'>0</Button>
+                <Button onclick={handleInput} name='1' value='1'>1</Button>
+                <Button onclick={handleInput} name='2' value='2'>2</Button>
+                <Button onclick={handleChange} name='3' value='3'>3</Button>
+                <Button onclick={handleAdd}>4</Button>
+                <Button onclick={handleSub}>5</Button>
+                <Button onclick={handleDiv}>6</Button>
+                <Button onclick={handleMul}>7</Button>
+                <Button onclick={handleMul}>8</Button>
+                <Button onclick={handleMul}>9</Button>
+            </div>
+
             <Answer>{answer}</Answer>
         </div>
     );
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,32 +2,8 @@ import React, { useState } from 'react'
 
 export default function Button(props) {
 
-    // let numberFirst = props.number1;
-    // let numberSecond = props.number2;
-    // let answer;
-
-    // const handleCalculation = () => {
-
-    //     switch (props.children) {
-    //         case "Add":
-    //             answer = +numberFirst + +numberSecond;
-    //             break;
-
-    //         case "Subtract":
-    //             answer = +numberFirst - +numberSecond;
-    //             break;
-
-    //         case "Multiply":
-    //             answer = +numberFirst * +numberSecond;
-    //             break;
-
-    //         case "Divide":
-    //             answer = +numberFirst / +numberSecond;
-    //             break;
-    //     }
-    // }
 
     return (
-        <button onClick={() => {props.onclick()}}>{props.children}</button>
+        <button onClick={(e) => {props.onclick(e)}} value={props.value}>{props.children}</button>
     )
 }
